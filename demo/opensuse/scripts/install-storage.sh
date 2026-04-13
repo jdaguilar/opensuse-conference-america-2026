537aaf472fe023843b302f11f0d103e9a0df107c
jdaguilar <jorgeaguilar2761@gmail.com>
2026-04-13 13:32:36 -0500

    feat: add storage layer

---
+++ demo/opensuse/scripts/install-storage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# scripts/install-storage.sh
+# [2/7] Deploys Storage (Apache Ozone & Filestash)
+
+set -e
+
+echo "[2/7] Deploying Storage (Apache Ozone & Filestash)..."
+
+# Ensure namespace exists
+kubectl create namespace data-storage --dry-run=client -o yaml | kubectl apply -f -
+
+# Add Helm Repo
+helm repo add apache https://apache.github.io/ozone-helm-charts/
+helm repo update
+
+# Install Ozone (Apache)
+helm upgrade --install ozone apache/ozone \
+  -f storage/ozone-values.yaml \
+  -n data-storage
+
+# Install Filestash (UI Tooling)
+kubectl apply -f storage/filestash.yaml
