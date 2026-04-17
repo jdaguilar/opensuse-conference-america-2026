06286c1437bf74ac15dfd5967fa1706ea9fafde0
jdaguilar <jorgeaguilar2761@gmail.com>
2026-04-17 15:48:34 -0500

    delete import

--- demo/opensuse/orchestration/dags/download_gh_archive.py
+++ demo/opensuse/orchestration/dags/download_gh_archive.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.decorators import task
-from airflow.utils.dates import days_ago
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
